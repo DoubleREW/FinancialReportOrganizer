7e665852afa677b2c4b1effcda3437f91e639d20
Fausto R <fausto91@gmail.com>
2022-08-01 17:02:51 +0200

    Initial Commit

---
+++ Shared/FinancialReportSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialReportSplitterApp.swift
+//  Shared
+//
+//  Created by Fausto Ristagno on 01/08/22.
+//
+
+import SwiftUI
+
+@main
+struct FinancialReportSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
