89ad6c523a9df1a5629fbe287d117b369ae18d96
Fausto R <fausto91@gmail.com>
2022-08-07 10:38:54 +0200

    Renamed ConnectBridge

---
+++ Shared/Models/AppStoreConnectBridge/AscAuthViewController.swift
@@ -0,0 +1,70 @@
+//
+//  AscAuthViewController.swift
+//  FinancialReportSplitter
+//
+//  Created by Fausto Ristagno on 05/08/22.
+//
+
+import Foundation
+import WebKit
+
+class AscAuthViewController : NSViewController {
+    var webView: WKWebView! {
+        didSet {
+            webView?.navigationDelegate = self
+        }
+    }
+    var onDismiss: ((Bool) -> Void)!
+
+    override init(nibName nibNameOrNil: NSNib.Name?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        view = NSView(frame: NSMakeRect(0.0, 0.0, 800, 600))
+
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(webView)
+
+        let button = NSButton(title: "Close", target: self, action: #selector(dismissSelf(_:)))
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+
+        NSLayoutConstraint.activate([
+            webView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1, constant: -40),
+            webView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1),
+
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10),
+        ])
+    }
+
+    @objc func dismissSelf(_ sender: Any?) {
+        self.dismissAndNotify(false)
+    }
+
+    func dismissAndNotify(_ isAuthenticated: Bool) {
+        self.view.window!.close()
+        self.onDismiss(isAuthenticated)
+    }
+}
+
+extension AscAuthViewController : WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse,
+                 decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+
+        if let response = navigationResponse.response as? HTTPURLResponse {
+            if let resUrl = response.url?.absoluteString, response.statusCode == 200 && resUrl.starts(with: "https://appstoreconnect.apple.com/itc/payments_and_financial_reports") {
+                self.dismissAndNotify(true)
+            }
+        }
+
+        decisionHandler(.allow)
+    }
+}
---
+++ Shared/Models/AppStoreConnectBridge/AscConnector.swift
@@ -0,0 +1,119 @@
+//
+//  AscConnector.swift
+//  FinancialReportSplitter
+//
+//  Created by Fausto Ristagno on 01/08/22.
+//
+
+import Foundation
+import WebKit
+
+struct UnauthorizedError : Error {}
+enum FetchError : Error {
+    case genericError
+    case fetchFailed
+    case invalidResponse
+    case decodingError
+    case requestFailed
+    case noData
+}
+
+class AscConnector : NSObject {
+    static var shared: AscConnector = {
+        return AscConnector()
+    }()
+
+    private static var loginUrl = "https://appstoreconnect.apple.com/login?targetUrl=%2Fitc%2Fpayments_and_financial_reports"
+    private static var userDetailUrl = "https://appstoreconnect.apple.com/WebObjects/iTunesConnect.woa/ra/user/detail"
+    private static var sapVendorNumbersUrl = " https://appstoreconnect.apple.com/WebObjects/iTunesConnect.woa/ra/paymentConsolidation/providers/%ld/sapVendorNumbers"
+    private static var reportsUrl = "https://appstoreconnect.apple.com/WebObjects/iTunesConnect.woa/ra/paymentConsolidation/providers/%ld/sapVendorNumbers/%ld?year=%ld&month=%ld"
+
+    private lazy var webView: WKWebView = {
+        return WKWebView()
+    }()
+
+    public private(set) var isAuthWindowPresented = false
+    public private(set) var isAuthenticated = false
+
+    func presentAuthWindow() {
+        let controller = AscAuthViewController(nibName: nil, bundle: nil)
+        controller.webView = self.webView
+        controller.onDismiss = { [weak self] isAuthenticated in
+            self?.isAuthenticated = isAuthenticated
+            self?.isAuthWindowPresented = false
+        }
+
+        let window = NSWindow(contentViewController: controller)
+        window.title = "App Store Connect Login"
+
+        let url = URL(string: Self.loginUrl)!
+        let request = URLRequest(url: url)
+        self.webView.load(request)
+
+        NSApp.mainWindow?.beginSheet(window)
+
+        self.isAuthWindowPresented = true
+    }
+
+    func fetchUserDetail() async throws -> UserDetail {
+        return try await self.fetchWebApi(Self.userDetailUrl, UserDetailResponse.self).payload
+    }
+
+    func fetchSapVendorNumbers(provider: Int) async throws -> [SapVendorNumber] {
+        let url = String(format: Self.sapVendorNumbersUrl, provider)
+
+        return try await self.fetchWebApi(url, SapVendorNumbersResponse.self).payload
+    }
+
+    func fetchReport(provider: Int, vendor: Int, month: Int, year: Int) async throws -> (report: FinancialReport, rawData: Data) {
+        let url = String(format: Self.reportsUrl, provider, vendor, year, month)
+        let res = try await self.fetchWebApi(url, FinancialReportResponse.self)
+
+        return (res.payload, res.rawData)
+    }
+
+    private func fetchWebApi<R: Response>(_ url: String, _ ofType: R.Type, requireAuth: Bool = true) async throws -> (payload: R.Payload, rawData: Data) {
+        guard !requireAuth || self.isAuthenticated else {
+            throw UnauthorizedError()
+        }
+
+        let script = "return await fetch(\"\(url)\").then(res => res.text()).catch(err => \"__FETCH_FAILED__\");"
+
+        let response: Any?
+        do {
+            response = try await self.webView.callAsyncJavaScript(script, contentWorld: .page)
+        } catch {
+            response = nil
+            throw FetchError.genericError
+        }
+
+        guard let responseJson = response as? String else {
+            throw FetchError.invalidResponse
+        }
+
+        guard responseJson != "__FETCH_FAILED__" else {
+            throw FetchError.fetchFailed
+        }
+
+        guard let responseData = responseJson.data(using: .utf8) else {
+            throw FetchError.invalidResponse
+        }
+
+        let responseObj: R
+        do {
+            responseObj = try JSONDecoder().decode(R.self, from: responseData)
+        } catch {
+            throw FetchError.decodingError
+        }
+
+        guard responseObj.statusCode == "SUCCESS" else {
+            throw FetchError.requestFailed
+        }
+
+        guard let responseObjData = responseObj.data else {
+            throw FetchError.noData
+        }
+
+        return (responseObjData, responseData)
+    }
+}
